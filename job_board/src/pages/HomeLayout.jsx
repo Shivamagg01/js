2d09ba5e41615e4472be855899d483673250828d
Shivamagg01 <shivamaggarwal3106@gmail.com>
2025-06-11 14:00:18 +0530

    my first commit

---
+++ job_board/src/pages/HomeLayout.jsx
@@ -0,0 +1,10 @@
+const HomeLayout = () => {
+  return (
+    <div>
+      <h1>Welcome to the Job Board</h1>
+      <h2>Your one-stop solution for job listings</h2>
+    </div>
+  )
+}
+
+export default HomeLayout;
